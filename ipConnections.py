538f6520343463aae837682cb6d584050f78a2d3
Mamaril-Jeremy <jmamaril@nevada.unr.edu>
2023-12-07 10:36:47 -0800

    ip

--- ipConnections.py
+++ ipConnections.py
@@ -1,14 +1,11 @@
 from scapy.all import rdpcap, wrpcap
 
 def clean_and_save_capture(input_file, output_file):
-    # Step 1: Read the Wireshark capture
     packets = rdpcap(input_file)
 
-    # Step 2: Filter packets (e.g., only TCP packets)
-    tcp_packets = [pkt for pkt in packets if pkt.haslayer('TCP')]
+    ip_packets = [pkt for pkt in packets if pkt.haslayer('IP')]
 
-    # Step 3: Create a new Scapy PacketList
-    new_capture = tcp_packets
+    new_capture = ip_packets
 
     # Step 4: Save the new capture to another file
     wrpcap(output_file, new_capture)
@@ -17,7 +14,7 @@ def clean_and_save_capture(input_file, output_file):
 input_file = 'clash_data.pcapng'
 
 # Replace 'cleaned_capture.pcap' with the desired output file name
-output_file = 'tcp_capture.pcapng'
+output_file = 'ip_capture.pcapng'
 
 # Call the function to clean and save the capture
 clean_and_save_capture(input_file, output_file)
